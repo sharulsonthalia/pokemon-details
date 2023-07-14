4a0496b25c255c4ca06e4dd9b6e3bca699960794
Sharul Sonthalia <sharulsonthalia@Sharuls-MacBook-Pro.local>
2023-07-14 15:42:56 -0400

    July 14 updates

--- pokemon.js
+++ pokemon.js
@@ -3,8 +3,9 @@ const getinfo = async() => {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon');
         const data = await response.json();
         const allPokemon = data.results;
-
+        console.log(allPokemon)
         render(allPokemon)
+
     } catch(err){
         const message = `sorry, error`
         render(message)
@@ -26,15 +27,41 @@ const render = (pokemonArr) => {
             const num = linkArr[i].dataset.pokemonNumber
             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
             const data = await response.json();
-            renderDetails(data)
+            renderImg(data)
         })
     }
 }
 
-const renderDetails = (pokemonData) => {
-    const renderedData = JSON.stringify(pokemonData, null, 2)
-    const section = document.querySelector(`section`)
-    section.innerText = renderedData
+const renderImg = (pokemonData) => {
+    const body = document.querySelector(`body`)
+ 
+    body.innerHTML = ``
+
+    const p = document.createElement(`p`)
+    p.innerHTML = `<h1>${pokemonData.name}</h1>`
+    body.appendChild(p)
+
+    body.innerHTML += `<img src = ${pokemonData.sprites.front_default} width= '200px'>`
+    
+    const space = document.createElement(`br`)
+    body.appendChild(space)
+
+    const button = document.createElement(`button`)
+    button.innerText = `Go Back!`
+    body.appendChild(button)
+
+    backbutton()
+}
+
+const backbutton = () => {
+    const button = document.querySelector(`button`)
+    const body = document.querySelector(`body`)
+    button.addEventListener(`click`, () => {
+        body.innerHTML= ``
+        body.innerHTML += `<h1>Pokemon Names</h1>`
+        body.innerHTML += `<ul></ul>`
+        getinfo()
+    })
 }
 
 
